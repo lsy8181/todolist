03592d5333b7eef233c74df86cedd184ab6287d1
lsy8181 <a4630486@gmail.com>
2024-05-16 10:57:19 +0900

    새 파일 생성

---
+++ my-react-app/src/App.jsx
@@ -0,0 +1,31 @@
+import { useState } from 'react';
+
+function App(){
+  const [toDo,setTodo] = useState("");
+  const [toDos,setTodos] = useState([]);
+  const onChange = (e) => {
+      setTodo(e.target.value)
+  }
+  const onSubmit = (e) => {
+    e.preventDefault();
+    if(toDo === ""){
+        return
+    }
+    setTodos((currentArray) =>[toDo, ...currentArray])
+    setTodo("")
+} 
+console.log(toDos) 
+return (
+  <div>
+      <h1>투두리스트를 만들어보자~</h1>
+      <form onSubmit={onSubmit}>
+          <input type="text" placeholder="입력하세요"  onChange={onChange} value={toDo}/>
+          <button>등록하기</button>
+      </form>
+      <ul>
+{toDos.map((item,index)=><li key={index}>{item}</li>)}
+      </ul>
+  </div>
+  )
+}
+export default App;
